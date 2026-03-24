b37e05190e838f9dd603ba33e8b9058e06e5431b
Andrew <demchuk162@gmail.com>
2026-03-24 22:06:51 +0200

    Add authentication endpoints for login, refresh, and logout

--- backend/app/api/auth.py
+++ backend/app/api/auth.py
@@ -1,7 +1,10 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, Response, Request
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.database import get_db
+from app.core.dependecies import get_current_user
+from app.models.user import User
+from app.schemas.token import LoginRequest, TokenResponse
 from app.schemas.user import UserResponse, UserCreate
 from app.services.auth import AuthService
 
@@ -12,3 +15,42 @@ auth_service = AuthService()
 async def register(user_data: UserCreate, db: AsyncSession = Depends(get_db)):
     return await auth_service.register(user_data, db)
 
+@router.post("/login", response_model=TokenResponse)
+async def login(form_data: LoginRequest,
+                response: Response,
+                db: AsyncSession = Depends(get_db)):
+    result = await auth_service.authenticate_user(form_data.username, form_data.password, db)
+    if result is None:
+        raise HTTPException(status_code=401, detail="Incorrect username or password", headers={"WWW-Authenticate": "Bearer"})
+    access = auth_service.create_access_token(result.id)
+    refresh = auth_service.create_refresh_token(result.id)
+    response.set_cookie(
+        key="refresh_token",
+        value=refresh,
+        httponly=True,
+        secure=True,
+        samesite="lax",
+        max_age=30 * 24 * 60 * 60)
+    return TokenResponse(access_token=access)
+
+@router.get("/me", response_model=UserResponse)
+async def get_me(current_user: User = Depends(get_current_user)):
+    return current_user
+
+@router.post("/refresh", response_model=TokenResponse)
+async def refresh(request: Request, db: AsyncSession = Depends(get_db)):
+    token = request.cookies.get("refresh_token")
+    if token is None:
+        raise HTTPException(status_code=401, detail="Invalid refresh token")
+    user = await auth_service.verify_refresh_token(token, db)
+    access_token = auth_service.create_access_token(user.id)
+    return TokenResponse(access_token=access_token)
+
+@router.post("/logout")
+async def logout(response: Response):
+    response.delete_cookie(
+        key="refresh_token",
+        httponly=True,
+        secure=True,
+        samesite="lax",)
+    return {"message": "Successfully logged out"}
--- backend/app/services/auth.py
+++ backend/app/services/auth.py
@@ -1,9 +1,12 @@
+from jose import jwt, JWTError
+from datetime import datetime, timedelta, timezone
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.models.user import User
 from app.schemas.user import UserCreate, UserResponse
 from fastapi import HTTPException, status
 from passlib.context import CryptContext
 from sqlalchemy import select
+from app.core.config import settings
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -36,4 +39,47 @@ class AuthService:
         await db.refresh(new_user)
         return UserResponse.model_validate(new_user)
 
+    async def authenticate_user(self, username: str, password: str, db: AsyncSession):
+        result = await db.execute(select(User).where(User.username == username))
+        user = result.scalar_one_or_none()
+        if user is None:
+            return None
+
+        if not pwd_context.verify(password, user.password):
+            return None
+
+        return user
+
+    def create_access_token(self, user_id: int) -> str:
+        utc_now = datetime.now(timezone.utc)
+        payload = {
+            "sub": str(user_id),
+            "exp": utc_now + timedelta(minutes=30)
+        }
+
+        return jwt.encode(payload, settings.SECRET_KEY, algorithm="HS256")
+
+    def create_refresh_token(self, user_id: int) -> str:
+        utc_now = datetime.now(timezone.utc)
+        payload = {
+            "sub": str(user_id),
+            "exp": utc_now + timedelta(days=30),
+            "type": "refresh"
+        }
+
+        return jwt.encode(payload, settings.SECRET_KEY, algorithm="HS256")
+
+    async def verify_refresh_token(self, token: str, db: AsyncSession) -> User:
+        try:
+            refresh = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
+        except JWTError:
+            raise HTTPException(status_code=400, detail="Invalid refresh token")
+        type = refresh["type"]
+        if type != "refresh":
+            raise HTTPException(status_code=400, detail="Invalid refresh token")
+        user = await db.execute(select(User).where(User.id == int(refresh["sub"])))
+        user = user.scalar_one_or_none()
+        if user is None:
+            raise HTTPException(status_code=400, detail="Invalid refresh token")
+        return user
 
