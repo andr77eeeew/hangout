be15158531c6b453a17dedafa06df22d5625c8d8
Andrew <demchuk162@gmail.com>
2026-03-24 22:07:25 +0200

    Add dependency for user authentication and current user retrieval

---
+++ backend/app/core/dependecies.py
@@ -0,0 +1,35 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt, JWTError
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.core.database import get_db
+from app.core.config import settings
+from app.models.user import User
+from sqlalchemy import select
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/user/login")
+
+async def get_current_user(
+        token: str = Depends(oauth2_scheme),
+        db: AsyncSession = Depends(get_db)
+) -> User:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        decode = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
+    except JWTError:
+        raise credentials_exception
+    try:
+        user_id = int(decode.get("sub"))
+        if user_id is None:
+            raise credentials_exception
+    except (ValueError, TypeError):
+        raise credentials_exception
+    result = await db.execute(select(User).where(User.id == int(user_id)))
+    user = result.scalar_one_or_none()
+    if user is None:
+        raise credentials_exception
+    return user
